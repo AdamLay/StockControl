fbbdd2bdb20d029da03c53fd913100559a622692
Adam Lay <adam93.lay+git@gmail.com>
2015-12-14 22:17:57 +0000

    Realtime fixes

--- StockControl/web/js/stock-list.js
+++ StockControl/web/js/stock-list.js
@@ -8,11 +8,24 @@ $(document).ready(function () {
     Inventory.StockAddEvent.Subscribe(function (data) {
         var html = '';
         html += '<li data-stockid="' + data.Id + '" class="list-group-item stock-item">';
-        html += '<button onclick="Inventory.IssueStock(' + data.Id + ')" title="Release 1 item from inventory" disabled="disabled" class="btn btn-primary pull-right"><i class="fa fa-gbp"></i> Issue</button>';
+        html += '<button onclick="Inventory.IssueStock(' + data.Id + ')" title="Release 1 item from inventory" ' + (data.Quantity < 1 ? 'disabled="disabled"' : '') + ' class="btn btn-primary pull-right">';
+        html += '<i class="fa fa-gbp"></i> Issue';
+        html += '</button>';
         html += '<h3 class="name">' + data.Name + '</h3>';
         html += '<div class="quantity">In Stock: ' + data.Quantity + '</div>';
         html += '</li>';
-        $('.list-group[data-groupid="' + data.StockGroupId + '"]').append(html);
+        var $list = $('.list-group[data-groupid="' + data.StockGroupId + '"]');
+        if ($list.length == 0) {
+            var newGroup = '';
+            newGroup += '<div class="panel-heading" data-groupid="' + data.StockGroupId + '">' + data.StockGroup + '</div>';
+            newGroup += '<ul class="list-group"  data-groupid="' + data.StockGroupId + '">';
+            html = newGroup + html;
+            html += '</ul>';
+            $("#stock").append(html);
+        }
+        else {
+            $list.append(html);
+        }
     });
     StockGroups.StockGroupUpdateEvent.Subscribe(function (group) {
         var $group = $(".panel-heading[data-groupid=\"" + group.Id + "\"]");
